59d117da7ec2a66b5e33c082e4280194c86e516a
henrylee746 <henry.lee746@gmail.com>
2025-12-07 21:26:32 -0500

    Made changes to footer including formatting and social icons, also a few changes to hero-parallax text animations and height decreased by -25vh

--- joca-app/src/app/events/EventCards.tsx
+++ joca-app/src/app/events/EventCards.tsx
@@ -76,7 +76,7 @@ export function EventCards() {
         </div>
       </section>
 
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 min-h-64">
+      <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 min-h-64">
         {error ? (
           <p>Error: {error.message}</p>
         ) : (
