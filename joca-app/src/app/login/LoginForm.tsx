43c7897af4a17df0ade2c80aee2dd8e93a89a2a1
henrylee746 <henry.lee746@gmail.com>
2026-02-24 23:05:40 -0500

    fix(router.push during render) Removed router.push statements that would've occured during client-side render

--- joca-app/src/app/login/LoginForm.tsx
+++ joca-app/src/app/login/LoginForm.tsx
@@ -82,7 +82,15 @@ export const LoginForm = () => {
     );
   }
 
-  if (session?.user) router.push("/");
+  if (session?.user)
+    return (
+      <div className="w-full h-full flex flex-col items-center justify-center p-8 gap-4">
+        <p className="text-center text-xl">Already logged in</p>
+        <Button variant="outline" asChild>
+          <Link href="/">Go to Home</Link>
+        </Button>
+      </div>
+    );
 
   if (!isMounted || isPending) return <Loading />;
 
--- joca-app/src/app/signup/SignupForm.tsx
+++ joca-app/src/app/signup/SignupForm.tsx
@@ -96,7 +96,16 @@ export function SignupForm() {
     );
   }
 
-  if (session?.user) router.push("/");
+  if (session?.user) {
+    return (
+      <div className="w-full h-full flex flex-col items-center justify-center p-8 gap-4">
+        <p className="text-center text-xl">Already logged in</p>
+        <Button variant="outline" asChild>
+          <Link href="/">Go to Home</Link>
+        </Button>
+      </div>
+    );
+  }
 
   if (
     isEmailVerificationPageVisible &&
